da073770d3422a7e3f2a4537bb29e3bbcb5970c4
Beltah Tech <beltahson@gmail.com>
2026-06-24 21:08:37 +0300

    Create App.tsx

---
+++ Lib/App.tsx
@@ -0,0 +1,21 @@
+import Header from './components/Header'
+import Hero from './components/Hero'
+import WhyChooseUs from './components/WhyChooseUs'
+import Services from './components/Services'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Header />
+      <Hero />
+      <WhyChooseUs />
+      <Services />
+      <Contact />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
